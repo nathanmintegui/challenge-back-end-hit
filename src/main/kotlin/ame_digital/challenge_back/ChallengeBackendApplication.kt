35c03d96f8c5047160cbdb3a0acb9316a8a42a7e
Nathan Berger <nathanberger.rs@hotmail.com>
2024-06-27 09:39:32 -0300

    add project template

---
+++ src/main/kotlin/ame_digital/challenge_back/ChallengeBackendApplication.kt
@@ -0,0 +1,10 @@
+package ame_digital.challenge_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class ChallengeBackendApplication
+
+fun main(args: Array<String>) {
+        runApplication<ChallengeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/ame_digital/challenge_back/ChallengeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ame_digital.challenge_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
