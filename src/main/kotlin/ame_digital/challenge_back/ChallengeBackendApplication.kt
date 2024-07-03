9ab577aab0548e33a468f8577dfd843391486a29
nathanmintegui <nathanberger.rs@hotmail.com>
2024-07-02 21:21:56 -0300

    feat: add delete endpoint

--- src/main/kotlin/ame_digital/challenge_back/ChallengeBackendApplication.kt
+++ src/main/kotlin/ame_digital/challenge_back/ChallengeBackendApplication.kt
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import jakarta.websocket.server.PathParam
 
 @SpringBootApplication class ChallengeBackendApplication
 
@@ -19,6 +22,11 @@ fun main(args: Array<String>) {
 data class CriarPlanetaRequest(val nome: String?, val clima: String?, val terreno: String?)
 
 data class Response(val message: String, val status: Int)
+
+object Constants{
+        const val NO_ROWS = 0,
+        const val MINIMUM_VALID_ID = 1
+}
 
 @RestController
 class PlanetaController(val db: JdbcTemplate) {
@@ -68,4 +76,28 @@ class PlanetaController(val db: JdbcTemplate) {
                 val res = Response(message = "criado com sucesso", status = 200)
                 return ResponseEntity(res, HttpStatus.OK)
         }
+
+        @DeleteMapping("/planetas/{id}")
+        fun deletar(@PathVariable id: Int): ResponseEntity<Response> {
+                if(id < Constants.MINIMUM_VALID_ID) {
+                        return ResponseEntity(
+                                Response(message = "id invalido", status = 400),
+                                HttpStatus.BAD_REQUEST
+                        )
+                }
+
+                val isPlanetaExistente = db.queryForObject<Int>(
+                        "SELECT COUNT(*) FROM PLANETAS WHERE id = ?", id)!!
+
+                if (isPlanetaExistente == Constants.NO_ROWS) {
+                        return ResponseEntity(
+                                Response(message = "planeta nao existe", status = 404),
+                                HttpStatus.BAD_REQUEST
+                        )
+                }
+
+                db.update("DELETE FROM planetas WHERE id = ?", id)
+
+                return ResponseEntity(Response(message = "", status = 204), HttpStatus.NO_CONTENT)
+    }
 }
