9812440c01926656f50eded1862abe39e1f7c42e
nathanmintegui <nathanberger.rs@hotmail.com>
2024-07-02 21:41:15 -0300

    feat: add get by id endpoint

--- src/main/kotlin/ame_digital/challenge_back/ChallengeBackendApplication.kt
+++ src/main/kotlin/ame_digital/challenge_back/ChallengeBackendApplication.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.GetMapping
 import jakarta.websocket.server.PathParam
 
 @SpringBootApplication class ChallengeBackendApplication
@@ -19,9 +20,16 @@ fun main(args: Array<String>) {
         runApplication<ChallengeBackendApplication>(*args)
 }
 
+class Planeta(
+        val id: Int,
+        val nome: String,
+        val clima: String
+        val terreno: String
+)
+
 data class CriarPlanetaRequest(val nome: String?, val clima: String?, val terreno: String?)
 
-data class Response(val message: String, val status: Int)
+data class Response(val message: String, val body: kotlin.Any?, val status: Int)
 
 object Constants{
         const val NO_ROWS = 0,
@@ -35,7 +43,8 @@ class PlanetaController(val db: JdbcTemplate) {
                 if (request.nome.isNullOrEmpty()) {
                         val responseBody =
                                         Response(
-                                                        message = "campo nome é obrigatório", 
+                                                        message = "campo nome é obrigatório",
+                                                        body = null,
                                                         status = 400
                                         )
 
@@ -46,6 +55,7 @@ class PlanetaController(val db: JdbcTemplate) {
                         val responseBody =
                                         Response(
                                                         message = "campo clima é obrigatório",
+                                                        body = null,
                                                         status = 400
                                         )
 
@@ -56,6 +66,7 @@ class PlanetaController(val db: JdbcTemplate) {
                         val responseBody =
                                         Response(
                                                         message = "campo terreno é obrigatório",
+                                                        body = null,
                                                         status = 400
  
                                                 )
@@ -73,7 +84,7 @@ class PlanetaController(val db: JdbcTemplate) {
                         // TODO: Error Handleing 
                 }
 
-                val res = Response(message = "criado com sucesso", status = 200)
+                val res = Response(message = "criado com sucesso", body = null, status = 200)
                 return ResponseEntity(res, HttpStatus.OK)
         }
 
@@ -81,7 +92,7 @@ class PlanetaController(val db: JdbcTemplate) {
         fun deletar(@PathVariable id: Int): ResponseEntity<Response> {
                 if(id < Constants.MINIMUM_VALID_ID) {
                         return ResponseEntity(
-                                Response(message = "id invalido", status = 400),
+                                Response(message = "id invalido", body = null, status = 400),
                                 HttpStatus.BAD_REQUEST
                         )
                 }
@@ -91,13 +102,49 @@ class PlanetaController(val db: JdbcTemplate) {
 
                 if (isPlanetaExistente == Constants.NO_ROWS) {
                         return ResponseEntity(
-                                Response(message = "planeta nao existe", status = 404),
+                                Response(message = "planeta nao existe", body = null, status = 404),
                                 HttpStatus.BAD_REQUEST
                         )
                 }
 
                 db.update("DELETE FROM planetas WHERE id = ?", id)
 
-                return ResponseEntity(Response(message = "", status = 204), HttpStatus.NO_CONTENT)
+                return ResponseEntity(
+                        Response(message = "", body = null, status = 204),
+                        HttpStatus.NO_CONTENT
+                )
     }
+
+        @GetMapping("/planetas/{id}")
+        fun listarPorId(@PathVariable id: Int): ResponseEntity<Response> {
+                if(id < Constants.MINIMUM_VALID_ID) {
+                        return ResponseEntity(
+                                Response(message = "id invalido", body = null, status = 400),
+                                HttpStatus.BAD_REQUEST
+                        )
+                }
+
+                var planeta = db.queryForObject(
+                "SELECT * FROM planetas WHERE id = ?",
+                id) {p , _ -> 
+                        Planeta(
+                                p.getInt("id"),
+                                p.getString("nome"),
+                                p.getString("clima"),
+                                p.getString("terreno")
+                        )
+                }
+
+                if(planeta is null) {
+                        return ResponseEntity(
+                                Response(message = "planeta nao existe", body = null, status = 404),
+                                HttpStatus.BAD_REQUEST
+                        )
+                }
+
+                return ResponseEntity(
+                        Response(message = "", body = planeta, status = 204),
+                        HttpStatus.NO_CONTENT
+                )
+        }
 }
